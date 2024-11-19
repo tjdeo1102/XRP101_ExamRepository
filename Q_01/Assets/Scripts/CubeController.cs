505d3c8d750d5df2fea94f33a74f566d52b9a345
tjdeo1102 <57614322+tjdeo1102@users.noreply.github.com>
2024-11-19 10:55:51 +0900

    1번 문제 제출

--- Q_01/Assets/Scripts/CubeController.cs
+++ Q_01/Assets/Scripts/CubeController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
-    public Vector3 SetPoint { get; private set; }
+    // SetPoint로 이동할 위치를 설정해야하지만 private로 접근 제한 되어 있음
+    //public Vector3 SetPoint { get; private set; }
+    public Vector3 SetPoint { get; set; }
 
     public void SetPosition()
     {
--- Q_01/Assets/Scripts/CubeManager.cs
+++ Q_01/Assets/Scripts/CubeManager.cs
@@ -11,6 +11,7 @@ public class CubeManager : MonoBehaviour
 
     private void Awake()
     {
+        // 큐브를 이동시킬 좌표를 사전에 설정
         SetCubePosition(3, 0, 3);
     }
 
@@ -24,13 +25,21 @@ public class CubeManager : MonoBehaviour
         _cubeSetPoint.x = x;
         _cubeSetPoint.y = y;
         _cubeSetPoint.z = z;
-        _cubeController.SetPosition();
+        // 할당되지 않은 _cubeController에서 SetPosition()이 호출됨
+        //_cubeController.SetPosition();
     }
 
     private void CreateCube()
     {
+        // 큐브 생성
         GameObject cube = Instantiate(_cubePrefab);
         _cubeController = cube.GetComponent<CubeController>();
-        _cubeSetPoint = _cubeController.SetPoint;
+
+        // 사전에 설정한 _cubeSetPoint를 _cubeController.SetPoint에 설정
+        //_cubeSetPoint = _cubeController.SetPoint;
+        _cubeController.SetPoint = _cubeSetPoint;
+
+        // 인스턴스화된 _cubeController에서 SetPosition()을 호출하여 위치를 이동해야함
+        _cubeController.SetPosition();
     }
 }
